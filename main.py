adcf1a7d0187ad39f2ee4782d615b76116411ee2
Ben Barwise <clacktronics@gmail.com>
2015-04-27 16:04:30 +0100

    cleared up junk and changed tabs to spaces

--- main.py
+++ main.py
@@ -1,63 +1,64 @@
 import pyb, ure, os
-from time import sleep			
+from time import sleep
 from pwm import ledpwm
 
 #=Utility=functions=======================================================================
+# These are functions that can be called in the interpreter after it loads they do useful
+# Functions to test parts are working.
 
 # Sets all LEDs to black
 def clear():
-	for led in pinconfig:
-		for pin in pinconfig[led]:
-			pinconfig[led][pin].pwm(0)
+  for led in pinconfig:
+    for pin in pinconfig[led]:
+      pinconfig[led][pin].pwm(0)
 
-# Sets one colour for all LEDs			
+# Sets one colour for all LEDs
 def color(color,pwm):
-	for led in pinconfig:
-		pinconfig[led][color].pwm(pwm)
+  for led in pinconfig:
+    pinconfig[led][color].pwm(pwm)
 
-# Rainbow function
+# walking rainbow pattern
 def rainbow(speed,repeats):
-	fademap = (100,20,10,1,0,1,10,20)
-	for i in range(0,repeats):
-		for step in range(0,8):
-			for led in range(0,8):			
-				pinconfig[(led+step)%8]['R'].pwm(fademap[led])
-				pinconfig[(led+step+4)%8]['G'].pwm(fademap[led])
-				pinconfig[(led+step+5)%8]['B'].pwm(fademap[led])
-			sleep(speed)
+  fademap = (100,20,10,1,0,1,10,20)
+  for i in range(0,repeats):
+    for step in range(0,8):
+      for led in range(0,8):
+      pinconfig[(led+step)%8]['R'].pwm(fademap[led])
+      pinconfig[(led+step+4)%8]['G'].pwm(fademap[led])
+      pinconfig[(led+step+5)%8]['B'].pwm(fademap[led])
+      sleep(speed)
 
-# Fades a chosen colour up a defined number of times ( checking balance )			
+# Fades a chosen colour up a defined number of times ( checking balance between leds )
 def fade(color,speed,repeats):
-	for intensity in range(0,101):
-		for led in pinconfig:
-			pinconfig[led][color].pwm(intensity)
-		sleep(speed)
-	
-			
-# Show horwitz colours in order on LEDs 1 to 8			
+  for intensity in range(0,101):
+    for led in pinconfig:
+      pinconfig[led][color].pwm(intensity)
+    sleep(speed)
+
+# Show horwitz colours in order on LEDs 1 to 8
 def horwitz():
-	for i in range(0,8):
-		pinconfig[i]['R'].pwm(colors[i+1]['R']) 
-		pinconfig[i]['G'].pwm(colors[i+1]['G'])  
-		pinconfig[i]['B'].pwm(colors[i+1]['B'])   
-		
-#=Horwitz=Colours=========================================================================		
+  for i in range(0,8):
+    pinconfig[i]['R'].pwm(colors[i+1]['R']) 
+    pinconfig[i]['G'].pwm(colors[i+1]['G'])  
+    pinconfig[i]['B'].pwm(colors[i+1]['B'])   
+
+#=Horwitz=Colours=========================================================================
 
 # These are the set colours
-	
-colors = 	{
-				0:{	'R':100,	'G':100,	'B':100	},
-				1:{	'R':100,	'G':98, 	'B':100	},
-				2:{	'R':100,	'G':100,	'B':0	},
-				3:{	'R':100,	'G':100,	'B':90	},
-				4:{	'R':70,	'G':100,	'B':60	},
-				5:{	'R':0,	'G':100,	'B':100	},
-				6:{	'R':0,	'G':90,	'B':100	},
-				7:{	'R':0,	'G':70,	'B':100	},
-				8:{	'R':100,	'G':20,	'B':100	}
-			}
-	
-#=LED=Setup===============================================================================	
+
+colors = { 
+0:{	'R':100, 'G':100, 'B':100 },
+1:{	'R':100, 'G':98,  'B':100 },
+2:{	'R':100, 'G':100, 'B':0   },
+3:{	'R':100, 'G':100, 'B':90  },
+4:{	'R':70,  'G':100, 'B':60  },
+5:{	'R':0,   'G':100, 'B':100 },
+6:{	'R':0,   'G':90,  'B':100 },
+7:{	'R':0,   'G':70,  'B':100 },
+8:{	'R':100, 'G':20,  'B':100 }
+}
+
+#=LED=Setup===============================================================================
 #Each array of LEDs can have their own frequency bar P18 which is LED 3 Red which is fixed
 
 RedF = 100
@@ -70,54 +71,59 @@ BlueF = 75
 # be there to not break the class assignment!
 
 pinconfig = {
-	0:{	'R':ledpwm('Y8',12,2,RedF),
-		'G':ledpwm('Y1',8,1,GreenF),
-		'B':ledpwm('Y3',10,1,BlueF)
-		},
-	1:{	'R':ledpwm('Y4',11,1,RedF),
-		'G':ledpwm('Y6',1,1,GreenF), #inv
-		'B':ledpwm('X9',4,1,BlueF)
-		},
-	2:{	'R':ledpwm('Y7',12,1,RedF),
-		'G':ledpwm('Y2',8,2,GreenF),
-		'B':ledpwm('X10',4,2,BlueF)
-		},
-	3:{	'R':ledpwm('P18',2,1,RedF),
-		'G':ledpwm('Y12',1,3,GreenF),
-		'B':ledpwm('X17',2,2,BlueF)
-		},
-	4:{	'R':ledpwm('X2',5,2,RedF),
-		'G':ledpwm('X3',9,1,GreenF),
-		'B':ledpwm('Y10',2,4,BlueF)
-		},
-	5:{	'R':ledpwm('X4',5,4,RedF),
-		'G':ledpwm('X6',0,0,GreenF),
-		'B':ledpwm('X8',14,1,BlueF)
-		},
-	6:{	'R':ledpwm('X7',13,1,RedF),
-		'G':ledpwm('X5',0,0,GreenF),
-		'B':ledpwm('Y9',2,3,BlueF)
-		},
-	7:{ 'R':ledpwm('X1',5,1,RedF),
-		'G':ledpwm('Y11',1,2,GreenF), #inv
-		'B':ledpwm('LED_YELLOW',2,1,BlueF) #inv
-		}
+0:{
+  'R':ledpwm('Y8',12,2,RedF),
+  'G':ledpwm('Y1',8,1,GreenF),
+  'B':ledpwm('Y3',10,1,BlueF)
+},
+1:{
+  'R':ledpwm('Y4',11,1,RedF),
+  'G':ledpwm('Y6',1,1,GreenF), #inv
+  'B':ledpwm('X9',4,1,BlueF)
+},
+2:{
+  'R':ledpwm('Y7',12,1,RedF),
+  'G':ledpwm('Y2',8,2,GreenF),
+  'B':ledpwm('X10',4,2,BlueF)
+},
+3:{
+  'R':ledpwm('P18',2,1,RedF),
+  'G':ledpwm('Y12',1,3,GreenF),
+  'B':ledpwm('X17',2,2,BlueF)
+},
+4:{
+  'R':ledpwm('X2',5,2,RedF),
+  'G':ledpwm('X3',9,1,GreenF),
+  'B':ledpwm('Y10',2,4,BlueF)
+},
+5:{
+  'R':ledpwm('X4',5,4,RedF),
+  'G':ledpwm('X6',0,0,GreenF),
+  'B':ledpwm('X8',14,1,BlueF)
+},
+6:{
+  'R':ledpwm('X7',13,1,RedF),
+  'G':ledpwm('X5',0,0,GreenF),
+  'B':ledpwm('Y9',2,3,BlueF)
+},
+7:{
+'R':ledpwm('X1',5,1,RedF),
+'G':ledpwm('Y11',1,2,GreenF), #inv
+'B':ledpwm('LED_YELLOW',2,1,BlueF) #inv
+}
 }
 
-#=Main=Program============================================================================			
+#=Main=Program============================================================================
 
 clear()
 
 sequences = [elem for elem in os.listdir('sequences') if elem[0] != '.']
 
 # Open the data text file, it runs through the sequence a line at a time, first it checks
-# if the line is 'commented' out with a # or if its a blank line to ignore. Then it checks it
-# starts with a number if yes then its a sequence step. If not its a command or title.
-# It works in boolean by seeing if an object is made when a match is correct.
-
-sequenceDivide = False
-seqStep = 0
-cache_sequence = ()
+# if the line is 'commented' out with a # or if its a blank line to ignore. Then it checks
+# it starts with a number if yes then its a sequence step. If not its a command or title.
+# It works in boolean by seeing if an object is made when a match is correct. Below are
+# the groups formed by the regular expression
 
 #group 0 : all
 #group 1 : all of the variable ( speed or loop )
@@ -134,33 +140,32 @@ cache_sequence = ()
 #group 12 : led6
 #group 13 : led7
 #group 14 : led8
-#group 15 : plain text
+#group 15 : all text
 
-# This loop opens the text file
-seqArray = []
-loadNextSeq = True
+# speed set to 0 so it rushes through the file until it is changed by setting the speed in
+# the file.
 speed = 0
 
 while True:
-	for sequence in sequences:
-		with open("sequences/%s" % sequence) as datatxtLine:
+  for sequence in sequences:
+    with open("sequences/%s" % sequence) as datatxtLine:
 
-			for lineNumber,line in enumerate(datatxtLine): 
-	
-					# Skip blank lines
-					if ure.match('#|\n',line) == None: 
-				
-						# Use regular expressions to compile information into variables
-						lineRead = ure.match(' *(((speed)|(loop)) *= *([\.0-9]*))?( *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9])*)?([- a-zA-Z0-9\(\)]*)',line)
-				
-						if lineRead.group(2) == 'speed':
-							speed = float(lineRead.group(5))
-						elif lineRead.group(2) == 'loop':
-							loop = lineRead.group(5)
-						elif lineRead.group(14) != None:
-							for i in range(0,8):
-								pinconfig[i]['R'].pwm(colors[int(lineRead.group(i+7))]['R']) 
-								pinconfig[i]['G'].pwm(colors[int(lineRead.group(i+7))]['G'])  
-								pinconfig[i]['B'].pwm(colors[int(lineRead.group(i+7))]['B'])
-							print('%s : Line - %d' % (sequence, lineNumber))
-							sleep(speed)
+      for lineNumber,line in enumerate(datatxtLine): 
+        
+        # Skip blank lines
+        if ure.match('#|\n',line) == None: 
+          
+          # Use regular expressions to compile information into variables
+          lineRead = ure.match(' *(((speed)|(loop)) *= *([\.0-9]*))?( *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9]) *([0-9])*)?([- a-zA-Z0-9\(\)]*)',line)
+          
+          if lineRead.group(2) == 'speed':
+            speed = float(lineRead.group(5))
+          elif lineRead.group(2) == 'loop':
+            loop = lineRead.group(5)
+          elif lineRead.group(14) != None:
+            for i in range(0,8):
+              pinconfig[i]['R'].pwm(colors[int(lineRead.group(i+7))]['R']) 
+              pinconfig[i]['G'].pwm(colors[int(lineRead.group(i+7))]['G'])  
+              pinconfig[i]['B'].pwm(colors[int(lineRead.group(i+7))]['B'])
+            print('%s : Line - %d' % (sequence, lineNumber))
+            sleep(speed)
--- pwm.py
+++ pwm.py
@@ -1,145 +1,41 @@
 from time import sleep
 import pyb
 
-
-
 class ledpwm:
-	
-	def __init__(self,pin,timer,channel,freq):
-		
-		self.freq = freq
-		self.timer = timer
-		self.channel = channel
-		self.pin = pin
-		
-		if pin == 'X5' or pin == 'X6':
-			self.dac = pyb.DAC(pin)
-			self.buf = bytearray(100)
-			self.dac.write_timed(self.buf, freq * len(self.buf), mode=pyb.DAC.CIRCULAR)
-			
-		elif pin == 'P18':
-			self.ch = pyb.LED(4)
-			
-		else:
-			self.pinpwm = pyb.Pin(pin)
-			timerset = pyb.Timer(self.timer, freq=self.freq)
-			self.ch = timerset.channel(self.channel, pyb.Timer.PWM, pin=self.pinpwm)
-	
-	def pwm(self,PWM):
-		if self.pin == 'X5' or self.pin == 'X6':
-			b = self.buf # cache buf variable for speed
-			pwm = PWM * len(b) // 100
-			for i in range(len(b)):
-				b[i] = 255 if i < pwm else 0
-		elif self.pin == 'P18':
-			PWM = self.map(PWM,0,100,0,256)
-			self.ch.intensity(PWM)
-		else:
-			if self.pin in ('Y6','Y11','Y12'):
-				PWM = int(self.map(PWM,0,100,100,0))
-			self.ch.pulse_width_percent(PWM)
-	
-	# Borrowed from Arduino
-	def map(self,x, in_min, in_max, out_min, out_max):
-		return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
-		
-		
-			
-			
-			
-			
-	
-	
-	
-	
-
-
-
-
-
-
-'''
-
-timer8 = pyb.Timer(8, freq=100)
-timer4 = pyb.Timer(4, freq=100)
-
-LED =  {
-1:
-{
-'R':timer8.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.Y2, pulse_width=0),
-'G':timer8.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.Y1, pulse_width=0),
-'B':timer4.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.Y3, pulse_width=0)
-}
-}
-
-while True:
-	for pulse in range(0,10000,5):
-		timer8.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.Y2, pulse_width=pulse)
-		sleep(.001)
-	timer8.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.Y2, pulse_width=0)	
-	for pulse in range(0,10000,5):
-		timer8.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.Y1, pulse_width=pulse)
-		sleep(.001)
-	timer8.channel(1, pyb.Timer.PWM, pin=pyb.Pin.board.Y1, pulse_width=0)	
-	for pulse in range(0,10000,5):
-		timer4.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.Y3, pulse_width=pulse)
-		sleep(.001)
-	timer4.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.Y3, pulse_width=0)	
-
-
-
-2:
-{
-'R':pyb.Pin('Y4', pyb.Pin.OUT_PP),
-'G':pyb.Pin('Y6', pyb.Pin.OUT_PP),
-'B':pyb.Pin( 'X9', pyb.Pin.OUT_PP)
-},
-
-3:
-{
-'R':pyb.Pin('Y7', pyb.Pin.OUT_PP),
-'G':pyb.Pin('Y8', pyb.Pin.OUT_PP),
-'B':pyb.Pin( 'X10', pyb.Pin.OUT_PP)
-},
-
-4:
-{
-'R':pyb.Pin('X11', pyb.Pin.OUT_PP),
-'G':pyb.Pin( 'X12', pyb.Pin.OUT_PP), 
-'B':pyb.Pin('X17' , pyb.Pin.OUT_PP)
-},
-
-5:
-{
-#'R':pyb.Pin('P19', pyb.Pin.OUT_PP),
-#'G':pyb.Pin('P18', pyb.Pin.OUT_PP),
-'B':pyb.Pin( 'X1', pyb.Pin.OUT_PP)
-},
-
-6:
-{
-'R':pyb.Pin('X2', pyb.Pin.OUT_PP),
-'G':pyb.Pin('X4', pyb.Pin.OUT_PP), 
-'B':pyb.Pin('X3', pyb.Pin.OUT_PP)
-},
-
-7:
-{
-'R':pyb.Pin('X6', pyb.Pin.OUT_PP),
-'G':pyb.Pin('X8', pyb.Pin.OUT_PP), 
-'B':pyb.Pin('Y9', pyb.Pin.OUT_PP) 
-},
-
-8:
-{
-'R':pyb.Pin('Y10', pyb.Pin.OUT_PP),
-'G':pyb.Pin('Y11', pyb.Pin.OUT_PP), 
-'B':pyb.Pin('Y12', pyb.Pin.OUT_PP)
-}
-}
-			
-			
-'''
-		
-
-
+  def __init__(self,pin,timer,channel,freq):
+  
+    self.freq = freq
+    self.timer = timer
+    self.channel = channel
+    self.pin = pin
+    
+    if pin == 'X5' or pin == 'X6':
+      self.dac = pyb.DAC(pin)
+      self.buf = bytearray(100)
+      self.dac.write_timed(self.buf, freq * len(self.buf), mode=pyb.DAC.CIRCULAR)
+      
+    elif pin == 'P18':
+      self.ch = pyb.LED(4)
+      
+    else:
+      self.pinpwm = pyb.Pin(pin)
+      timerset = pyb.Timer(self.timer, freq=self.freq)
+      self.ch = timerset.channel(self.channel, pyb.Timer.PWM, pin=self.pinpwm)
+  
+  def pwm(self,PWM):
+    if self.pin == 'X5' or self.pin == 'X6':
+      b = self.buf # cache buf variable for speed
+      pwm = PWM * len(b) // 100
+        for i in range(len(b)):
+          b[i] = 255 if i < pwm else 0
+    elif self.pin == 'P18':
+      PWM = self.map(PWM,0,100,0,256)
+      self.ch.intensity(PWM)
+    else:
+      if self.pin in ('Y6','Y11','Y12'):
+        PWM = int(self.map(PWM,0,100,100,0))
+      self.ch.pulse_width_percent(PWM)
+  
+# Borrowed from Arduino
+  def map(self,x, in_min, in_max, out_min, out_max):
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
